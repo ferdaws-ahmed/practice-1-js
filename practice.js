a7ad40bcd40f585ec45c916cd983de466cd8a4d8
ferdaws-ahmed <mdferdawsahmeed@gmail.com>
2024-12-22 00:56:13 +0600

    update

--- practice.js
+++ practice.js
@@ -1,3 +1,31 @@
 
+//   function --->
+
+    function doubleIt( num ){
+        var result = num * 2;
+        return result;
+       
+
+    }
+    var num1 = doubleIt(50);
+    var num2 = doubleIt(100);
+    var total = num1 + num2;
+   
+    console.log(num1, num2);
+    console.log(total);
+ 
+
+
+     function add(num1, num2, num3){
+        var result = (num1 + num2) * num3;
+        return result;
+     }
+     var total = add(50, 20, 2);
+     console.log(total);
+
+    
+
+
+
 
 
