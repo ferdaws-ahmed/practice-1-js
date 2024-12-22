9aa07365a8a369e5f333f1c6ac3ba3a995fe2c95
ferdaws-ahmed <mdferdawsahmeed@gmail.com>
2024-12-22 12:43:40 +0600

    incToFeet function add

--- practice.js
+++ practice.js
@@ -1,29 +1,7 @@
 
-//   function --->
 
-    function doubleIt( num ){
-        var result = num * 2;
-        return result;
-       
+// function
 
-    }
-    var num1 = doubleIt(50);
-    var num2 = doubleIt(100);
-    var total = num1 + num2;
-   
-    console.log(num1, num2);
-    console.log(total);
- 
-
-
-     function add(num1, num2, num3){
-        var result = (num1 + num2) * num3;
-        return result;
-     }
-     var total = add(50, 20, 2);
-     console.log(total);
-
-    
 
 
 
